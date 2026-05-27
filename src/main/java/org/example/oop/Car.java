0599ef3f4b4f15e9cce737b0b48dd5e497158050
Suhira30 <suhirabalarajan@gmail.com>
2026-05-27 18:39:36 +0530

    feat:car.java class created

---
+++ src/main/java/org/example/oop/Car.java
@@ -0,0 +1,16 @@
+package org.example.oop;
+
+public class Car extends Vehicle {
+
+    String model;
+    String color;
+
+    public Car() {
+        super();
+// this no arg constructor internally call super() -> this super() call no arg constructor of parent
+    }
+
+    public void honk() {
+        System.out.println("car honk");
+    }
+}
